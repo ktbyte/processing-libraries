6b5b10e9498919bc0723b19d1f6f498b9f1a75e6
Ovidiu Miu <ovidiu.m94@gmail.com>
2018-05-26 17:24:20 +0300

    modified SoundCipher classes and static webpages - added playChord(), changeInstrumnet() and loadInstrument() methods

---
+++ ktbyte-library/src/ktbyte/midi/Instruments.java
@@ -0,0 +1,18 @@
+package ktbyte.midi;
+
+public interface Instruments {
+    public static final float PIANO = 0;
+    public static final float XYLOPHONE = 13;
+    public static final float ELECTRIC_GUITAR = 27;
+    public static final float ACOUSTIC_BASS = 32;
+    public static final float STRINGS = 48;
+    public static final float ORCHESTRA_HIT = 55;
+    public static final float TRUMPET = 56;
+    public static final float TUBA = 58;
+    public static final float BRASS = 61;
+    public static final float ALTO_SAX = 65;
+    public static final float CLARINET = 71;
+    public static final float FLUTE = 73;
+    public static final float TAIKO = 116;
+    public static final float SYNTH_DRUM = 118;
+}
