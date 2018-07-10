75f797da32e7ac993ac437e4b34a6119fe65a27f
Samuil9999 <freelancr@ukr.net>
2018-07-11 00:45:03 +0300

    Modified KTGUIConsole class
    Added private classes 'ScrollBar' and 'DirectionButton'

--- ktbyte-library/src/ktgui/KTGUIConsole.java
+++ ktbyte-library/src/ktgui/KTGUIConsole.java
@@ -150,4 +150,93 @@ public class KTGUIConsole extends Controller {
 			textBlock = getBlock(getBlockCount() - 1);
 		return textBlock;
 	}
+
+	private class ScrollBar extends Controller {
+		DirectionButton scrollUpButton, scrollDownButton;
+		
+		public ScrollBar(KTGUI ktgui, String title, int posx, int posy, int w, int h) {
+			super(ktgui);
+
+			this.title = title;
+			this.posx = posx;
+			this.posy = posy;
+			this.w = w;
+			this.h = h;
+
+			pg = pa.createGraphics(w + 1, h + 1);
+			userpg = pa.createGraphics(w + 1, h + 1);
+
+			createButtons();
+			
+			// automatically register the newly created window in default stage of stageManager
+			StageManager.getInstance().getDefaultStage().registerController(this);	
+		}
+
+		private void createButtons() {
+			scrollUpButton = new DirectionButton(ktgui, "db-up:" + title, posx, posy, w, w);
+			scrollUpButton.setDirection(UP);
+			scrollUpButton.addEventAdapter(new KTGUIEventAdapter() {
+				public void onMousePressed() {
+					System.out.println(scrollUpButton.title + " pressed.");
+				}
+			});
+			attachController(scrollUpButton);
+			registerChildController(scrollUpButton);
+			
+			scrollDownButton = new DirectionButton(ktgui, "db-down:" + title, posx, posy - w, w, w);
+			scrollDownButton.setDirection(DOWN);
+			scrollDownButton.addEventAdapter(new KTGUIEventAdapter() {
+				public void onMousePressed() {
+					System.out.println(scrollDownButton.title + " pressed.");
+				}
+			});
+			attachController(scrollDownButton);
+			registerChildController(scrollDownButton);
+		}
+		
+	}
+	
+	private class DirectionButton extends Button {
+
+		int direction = UP;
+		
+		public DirectionButton(KTGUI ktgui, String title, int posx, int posy, int w, int h) {
+			super(ktgui, title, posx, posy, w, h);
+		}
+
+		public void updateGraphics() {
+			pg.beginDraw();
+			pg.rectMode(CORNER);
+			if (isHovered && !isPressed) {
+				pg.fill(KTGUI.COLOR_FG_HOVERED);
+			} else if (isHovered && isPressed) {
+				pg.fill(KTGUI.COLOR_FG_PRESSED);
+			} else {
+				//pg.fill(ktgui.COLOR_FG_PASSIVE);
+				pg.fill(200, 200);
+			}
+			pg.stroke(0);
+			pg.strokeWeight(1);
+			pg.rect(0, 0, w, h, r1, r2, r3, r4);
+			pg.pushMatrix();
+			pg.translate(w * 0.5f, h * 0.5f);
+			if(direction == UP) {
+				pg.rotate(PI);
+			} else if (direction == RIGHT) {
+				pg.rotate(PI + HALF_PI);
+			} else if (direction == DOWN) {
+				pg.rotate(0);
+			} else if (direction == LEFT) {
+				pg.rotate(HALF_PI);
+			}
+			pg.line(-0.4f * w, -0.4f * h, 0, 0.4f * h);
+			pg.line(0.4f * w, -0.4f * h, 0, 0.4f * h);
+			pg.popMatrix();
+			pg.endDraw();
+		}
+		
+		public void setDirection(int direction) {
+			this.direction = direction;
+		}		
+	}
 }
