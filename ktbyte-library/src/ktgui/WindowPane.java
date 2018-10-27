46789af23e7a098db9b53e749064b875103f5467
ivanshuba <shubaivan@gmail.com>
2018-10-27 21:11:14 +0300

    Minor changes

--- ktbyte-library/src/ktgui/WindowPane.java
+++ ktbyte-library/src/ktgui/WindowPane.java
@@ -3,11 +3,11 @@ package ktgui;
 class WindowPane extends Pane {
 	Window parentWindow;
 
-	WindowPane(KTGUI ktgui, Window window, int posx, int posy, int w, int h) {
-		super(ktgui, posx, posy, w, h);
-		this.parentWindow = window;
-		//isDragable = false;
-	}
+//	WindowPane(KTGUI ktgui, Window window, int posx, int posy, int w, int h) {
+//		super(ktgui, posx, posy, w, h);
+//		this.parentWindow = window;
+//		//isDragable = false;
+//	}
 
 	WindowPane(KTGUI ktgui, String title, Window window, int posx, int posy, int w, int h) {
 		super(ktgui, title, posx, posy, w, h);
